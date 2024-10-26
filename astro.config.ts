2e6f0a4c6f7b41096ad4fcae3ad82516f924178c
Davi Reis <dreisss.dev@gmail.com>
2024-10-25 21:18:18 -0300

    feat: add lucide solid

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config'
 import alpinejs from '@astrojs/alpinejs'
 import tailwind from '@astrojs/tailwind'
 
+import solidJs from '@astrojs/solid-js'
+
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), solidJs()],
 })
